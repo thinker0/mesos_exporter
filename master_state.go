b8860a029568ec33a6e3937df2e61b834d25c466
thinker0 <thinker0@gmail.com>
2019-01-01 16:27:24 +0900

    log debug info

--- master_state.go
+++ master_state.go
@@ -13,7 +13,6 @@ import (
 type (
 	slave struct {
 		Active     bool
-		Hostname   string
 		PID        string                     `json:"pid"`
 		Used       resources                  `json:"used_resources"`
 		Unreserved resources                  `json:"unreserved_resources"`
